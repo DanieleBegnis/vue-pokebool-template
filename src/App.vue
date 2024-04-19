0e27679452ebe437be750b6959f38af7c0324a5a
f.giorgini <f.giorgini@dishcovery.menu>
2024-04-19 15:40:56 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from 'axios';
+import { store } from './store.js';
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      store
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<template>
+  <HelloWorld></HelloWorld>
+</template>
+
+<style lang="scss">
+  @use './style/generic.scss';
+  @use './style/partials/variables' as *;
+
+
+</style>
